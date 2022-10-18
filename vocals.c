c7102b6c1936ec30066920d7f71d1800bff8d1d1
Carlos Veryan <carlosveryan4@gmail.com>
2022-10-18 13:57:43 +0000

    Practica 3

---
+++ vocals.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <unistd.h>
+
+
+int main(){
+    char c;
+    char may;
+    int n = 0;
+    int count = 0;
+
+    do{
+        n = read(STDIN_FILENO,&c,1);
+        may = toupper(c);
+        if(may == 'A' || may == 'E' ||may == 'I' ||may == 'O' ||may == 'U') {
+            count++;
+        }
+    } while(n != 0);
+    printf("%d", count);
+    return 0;
+}
